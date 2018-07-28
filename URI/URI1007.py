934c8fcb15078dcc3ae8ecdb15c38c1adcbc7bf6
Bruna Rychelle <b.rychelle@gmail.com>
2018-07-28 18:32:31 -0300

    Resolve URI 1007

---
+++ URI/URI1007.py
@@ -0,0 +1,10 @@
+# Recebe quatro inteiros e retorna a diferenca do produto de dois pares
+
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+
+dif = ((a * b) - (c * d))
+
+print("DIFERENCA = " + str(dif))
