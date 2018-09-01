d0970c5dfca0fd985a38c1cd4a57b5ea3326ca5e
Bruna Rychelle <b.rychelle@gmail.com>
2018-09-01 13:09:23 -0300

    Resolve L 01 Q09 - PPZ

---
+++ Python para Zumbis/Lista I/L01 Q09.py
@@ -0,0 +1,8 @@
+# Pergunte a quantidade de km percorridos por um carro alugado pelo usuário, assim como 
+# a quantidade de dias pelos quais o carro foi alugado.
+# Calcule o preço a pagar, sabendo que o carro custa R$ 60,00 por dia e R$ 0,15 por km rodado.
+
+km = float(input("Quantos km foram percorridos? ")) * 0.15
+dias = int(input("Quantos dias de aluguel? ")) * 60
+
+print("Preço a pagar: R$ %.2f" %(km + dias))
